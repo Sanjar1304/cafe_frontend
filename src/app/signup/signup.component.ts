2eadd61d6e831a808576f2b5bebf4d8594e9f532
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-20 18:31:44 +0500

    Started signup dialog template

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { GlobalConstants } from '../shared/global-constant';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Router } from '@angular/router'
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +15,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: any = FormGroup;
+  responseMessage: any;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private userService: UserService,
+              private snackBar: SnackbarService,
+              private dailogRef: MatDialogRef<SignupComponent>,
+              private ngxService: NgxUiLoaderService) { }
+
 
   ngOnInit(): void {
+    this.signUpFormValidation();
   }
 
+
+  signUpFormValidation(){
+    this.signupForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: ['', [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber: ['', [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]]
+    })
+  }
+
+
+  handleSubmit(){
+    this.ngxService.start();
+    var formData = this.signupForm.value;
+    var data = {
+      name: formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      password: formData.password
+    };
+
+    this.userService.signup(data).subscribe((response:any) => {
+      this.ngxService.stop();
+      this.dailogRef.close();
+      this.responseMessage = response?.message;
+      this.snackBar.openSnackBar(this.responseMessage, '');
+      this.router.navigate(['/']);
+    },
+    (error) => {
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackBar.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
 }
