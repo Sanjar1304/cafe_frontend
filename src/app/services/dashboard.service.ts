f7875381f36eeed855b15433c87faa1b44195682
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-26 12:41:44 +0500

    Added dashboard service

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  url = environment.apiUrl
+
+  constructor() { }
+}
