3aa9d9588c0f4cd19eddc823847457da786bade1
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-26 12:16:48 +0500

    Added checkToken  to user service and home com

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,9 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
 import { LoginComponent } from '../login/login.component';
+import { Router } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +14,13 @@ import { SignupComponent } from '../signup/signup.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog,
+              private router: Router,
+              private userService: UserService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.checkTokenAction()
+  }
 
   signupAction(){
     const dialogConfig = new MatDialogConfig();
@@ -32,6 +38,17 @@ export class HomeComponent implements OnInit {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = '550px';
     this.dialog.open(LoginComponent, dialogConfig)
+  }
+
+
+  checkTokenAction(){
+    if(localStorage.getItem('token') != null){
+      this.userService.checkToken().subscribe((response: any) => {
+        this.router.navigate(['/cafe/dashboard']);
+      }, (error: any) => {
+        console.log(error);
+      })
+    }
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -30,4 +30,9 @@ export class UserService {
       headers: new HttpHeaders().set('Content-Type', 'application/json')
     })
   }
+
+
+  checkToken(){
+    return this.httpClient.get(`${this.url}/user/checkToken`);
+  }
 }
