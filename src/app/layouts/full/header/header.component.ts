6934d84cdbf4b1a368701bb104b141ad6f602d55
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-03 16:38:53 +0500

    Finished logout method in header component

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { ChangePasswordComponent } from 'src/app/material-component/dialog/change-password/change-password.component';
+import { Component } from '@angular/core';
+import { ConfirmationComponent } from 'src/app/material-component/dialog/confirmation/confirmation.component';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,8 +13,27 @@ import { Router } from '@angular/router';
 export class AppHeaderComponent {
   role: any;
   constructor(private router: Router,
-    private dialog: MatDialog) {
+              private dialog: MatDialog) {}
 
+  logout(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    dialogConfig.data = {
+      message: 'Logout'
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user) => {
+      dialogRef.close();
+      localStorage.clear();
+      this.router.navigate(['/'])
+    })
+  }
+
+
+  changePassword(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(ChangePasswordComponent, dialogConfig)
   }
 
 }
