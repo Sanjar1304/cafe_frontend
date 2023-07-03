4e800f92c32eb5b2450d5a5b80d891f07084bd3d
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-03 18:10:55 +0500

    Started changePassword method

--- src/app/material-component/dialog/change-password/change-password.component.ts
+++ src/app/material-component/dialog/change-password/change-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  changePasswordForm:any = FormGroup;
+  responseMessage: any
+
+  constructor(private fb: FormBuilder,
+              private userService: UserService,
+              private snackbar: SnackbarService,
+              public dialogRef: MatDialogRef<ChangePasswordComponent>,
+              private ngxService: NgxUiLoaderService) { }
 
   ngOnInit(): void {
+    this.changePasswordFormValidation();
+  }
+
+
+  changePasswordFormValidation(){
+    this.changePasswordForm = this.fb.group({
+      oldPassword: [null, [Validators.required]],
+      newPassword: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]]
+    })
+  }
+
+  validateSubmit(){
+    if(this.changePasswordForm.controls['newPassword'].value){
+
+    }
   }
 
 }
