dee77a5ad325bbdb4c1f1674ef9f9406c6cdc2cf
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-04 11:46:51 +0500

    Finished change-password component template

--- src/app/material-component/dialog/change-password/change-password.component.ts
+++ src/app/material-component/dialog/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { GlobalConstants } from 'src/app/shared/global-constant';
 import { MatDialogRef } from '@angular/material/dialog';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { SnackbarService } from 'src/app/services/snackbar.service';
@@ -35,12 +36,45 @@ export class ChangePasswordComponent implements OnInit {
     })
   }
 
+  get oldPassword(){
+    return this.changePasswordForm.get('oldPassword');
+  }
+
+  get newPassword(){
+    return this.changePasswordForm.get('newPassword');
+  }
+
+  get confirmPassword(){
+    return this.changePasswordForm.get('confirmPassword');
+  }
+
   validateSubmit(){
    (this.changePasswordForm.controls['newPassword'].value != this.changePasswordForm.controls['confirmPassword'].value) ? true : false;
   }
 
   handleChangePassword(){
-
+    this.ngxService.start();
+    var formData = this.changePasswordForm.value;
+    let data = {
+      oldPassword: formData.oldPassword,
+      newPassword: formData.newPassword,
+      confirmPassword: formData.confirmPassword
+    }
+    this.userService.changePassword(data).subscribe((response: any) => {
+      this.ngxService.stop();
+      this.responseMessage = response?.message;
+      this.dialogRef.close();
+      this.snackbar.openSnackBar(this.responseMessage, 'success');
+    }, (error) => {
+      console.log(error);
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage = error?.error?.message;
+      }else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbar.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
   }
 
 }
