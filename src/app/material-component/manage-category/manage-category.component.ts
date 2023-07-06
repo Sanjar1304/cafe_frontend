c687860df36b683805e221ead4503a72a6a1b214
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-06 16:49:32 +0500

    Added table and search bar to manage-category

--- src/app/material-component/manage-category/manage-category.component.ts
+++ src/app/material-component/manage-category/manage-category.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoryService } from 'src/app/services/category.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+
 @Component({
   selector: 'app-manage-category',
   templateUrl: './manage-category.component.html',
@@ -7,9 +15,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCategoryComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'edit'];
+  dataSource: any;
+  responseMessage: any;
+
+  constructor(private categoryService: CategoryService,
+              private ngxService: NgxUiLoaderService,
+              private dialog: MatDialog,
+              private snackbar: SnackbarService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
   }
+
+
+  tableData(){
+    this.categoryService.getCategory().subscribe((response: any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response)
+    }, (error) => {
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbar.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handleAddAction(){
+
+  }
+
+  handleEditAction(value:any){}
 
 }
