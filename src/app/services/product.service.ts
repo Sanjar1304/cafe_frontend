76e256f231ac649c1ed1b1f09299dd41c4bca967
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-08 12:54:24 +0500

    Added update and add methods to product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  url = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  addProduct(data: any){
+    return this.httpClient.post(`${this.url}/product/add`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    })
+  }
+
+
+  updateProduct(data: any){
+    return this.httpClient.patch(`${this.url}/product/update`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    })
+  }
+
 }
