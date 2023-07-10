8d1e14d98ab530c3ae053b91740f4f19d493f2dd
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-10 17:17:49 +0500

    Added private fields  in manage-product

--- src/app/material-component/manage-product/manage-product.component.ts
+++ src/app/material-component/manage-product/manage-product.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+
 @Component({
   selector: 'app-manage-product',
   templateUrl: './manage-product.component.html',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageProductComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'categoryName', 'description', 'price', 'edit'];
+  dataSource: any;
+  responseMessage: any;
+
+  constructor(private ngxService: NgxUiLoaderService,
+              private productService: ProductService,
+              private dialog: MatDialog,
+              private snackbar: SnackbarService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    // this.ngxService.start();
+    this.tableData();
+  }
+
+  tableData(){
+
   }
 
 }
