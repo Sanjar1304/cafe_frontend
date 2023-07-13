ad9d3110f22f4fa1e62d463dcaeeb30e60b9f5af
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-13 17:51:17 +0500

    Added httpClient to bill service

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
 
-  constructor() { }
+  url = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) {}
+
+
+  
 }
