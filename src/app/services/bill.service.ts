0ca21ae964ce06f166e56db58c5ea5dfde6308ee
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-14 09:50:52 +0500

    Added file-saver and method to bill service

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,5 +14,18 @@ export class BillService {
   constructor(private httpClient: HttpClient) {}
 
 
-  
+  generateReport(data: any){
+    return this.httpClient.post(`${this.url}/bill/generateReport/`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    })
+  }
+
+
+
+  getPdfReport(data: any): Observable<Blob>{
+    return this.httpClient.post(`${this.url}/bill/getPdf`, data, {responseType: 'blob'})
+  }
+
+
+
 }
