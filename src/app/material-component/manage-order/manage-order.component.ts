340d70c3cc7df19fa0035c11a431e7f59805c32a
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-14 12:03:32 +0500

    Added private fields to constructor and validation

--- src/app/material-component/manage-order/manage-order.component.ts
+++ src/app/material-component/manage-order/manage-order.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { BillService } from 'src/app/services/bill.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ProductService } from 'src/app/services/product.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-manage-order',
@@ -7,9 +17,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageOrderComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'category', 'price', 'quantity', 'total', 'edit'];
+  dataSource: any = [];
+  manageOrderForm: any = FormGroup;
+  categoryList: any = [];
+  products: any = [];
+  price: any;
+  totalAmount: number = 0;
+  responseMessage: any;
+
+  constructor(private ngxService: NgxUiLoaderService,
+              private billService: BillService,
+              private categoryService: CategoryService,
+              private productService: ProductService,
+              private snackBar: SnackbarService,
+              private dialog: MatDialog,
+              private fb: FormBuilder) { }
+
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.manageOrderFormValidation();
   }
+
+
+  manageOrderFormValidation(){
+    this.manageOrderForm = this.fb.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber: [null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      paymentMethod: [null, [Validators.required]],
+      product: [null, [Validators.required]],
+      category: [null, [Validators.required]],
+      quantity: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+      total: [0, [Validators.required]],
+    })
+  }
+
+
+
+
+
+
+
+
+
 
 }
