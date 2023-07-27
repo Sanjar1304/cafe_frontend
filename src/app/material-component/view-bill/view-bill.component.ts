607a3267bec8af84528036add48c97e02d5ba26b
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-27 09:39:16 +0500

    Added methods to view-bill component

--- src/app/material-component/view-bill/view-bill.component.ts
+++ src/app/material-component/view-bill/view-bill.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { BillService } from 'src/app/services/bill.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { ViewBillProductsComponent } from '../dialog/view-bill-products/view-bill-products.component';
 
 @Component({
   selector: 'app-view-bill',
@@ -7,9 +16,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBillComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'email', 'contactNumber', 'paymentMethod', 'total', 'view'];
+  dataSource: any;
+  responseMessage: any;
+
+  constructor(private ngxService: NgxUiLoaderService,
+              private billService: BillService,
+              private snackbar: SnackbarService,
+              private dialog: MatDialog,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
   }
+
+  tableData(){
+    this.billService.getBills().subscribe((response: any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+    }, (error: any) => {
+      this.ngxService.stop();
+      if(error.error.message) {
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbar.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
+
+
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filer = filterValue.trim().toLowerCase();
+  }
+
+
+
+  handleViewAction(values: any){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'View'
+    }
+    dialogConfig.width = '100%';
+    const dialogRef = this.dialog.open(ViewBillProductsComponent, dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    })
+  }
+
+
+
 
 }
