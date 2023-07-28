64d786c95c9a6cff56a0e408cf1bfb3be782ec41
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-28 10:56:51 +0500

    Added table to manage-user component

--- src/app/material-component/manage-user/manage-user.component.ts
+++ src/app/material-component/manage-user/manage-user.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GlobalConstants } from 'src/app/shared/global-constant';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { UserService } from 'src/app/services/user.service';
+
 @Component({
   selector: 'app-manage-user',
   templateUrl: './manage-user.component.html',
@@ -7,9 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUserComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'email', 'contactNumber', 'status'];
+  dataSource: any;
+  responseMessage: any;
+
+  constructor(private ngxService: NgxUiLoaderService,
+              private userService: UserService,
+              private snackbar: SnackbarService) { }
+
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
   }
+
+
+  tableData(){
+    this.userService.getUsers().subscribe((response: any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+    }, (error:any) => {
+      this.ngxService.stop();
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbar.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
+
+
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+
+  handleChangeAction(status: any, id: any){
+    this.ngxService.start();
+    var data = {
+      status: status.toString(),
+      id: id
+    }
+
+    this.userService.update(data).subscribe((response:any) => {
+      this.ngxService.stop();
+      this.responseMessage = response?.message;
+      this.snackbar.openSnackBar(this.responseMessage, 'success');
+    }, (error: any) => {
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbar.openSnackBar(this.responseMessage, GlobalConstants.error)
+    })
+  }
+
+
+
+
 
 }
